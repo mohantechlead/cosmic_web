4faee93fd31627a8f74d6ac6893de30e8fc83da4
tibarekb <berassut@gmail.com>
2024-06-21 10:27:01 +0300

    side bar added

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns=[
     path("user_base",views.user_base, name="user_base"),
+    path("create_customer",views.create_customer, name="create_customer"),
 ]
 
--- users/views.py
+++ users/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def user_base(request):
     return render(request,'user_base.html')
 
+def create_customer(request):
+    return render(request,'create_customer.html')
 
